3b0e3728b1e1baf048dc44c62140f40f85a8bc3b
Serenity Williams <swilliams314159@gmail.com>
2024-05-15 17:57:19 -0400

    fixed and updated player movement, added flag action, dig action

--- Assets/scripts/player_behavior.cs
+++ Assets/scripts/player_behavior.cs
@@ -11,108 +11,212 @@ using System;
 using UnityEngine.UIElements;
 using System.Xml.Schema;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 
 public class player_behavior : MonoBehaviour
 {
-    [SerializeField]
-    private Tilemap grassMap;
-    [SerializeField]
-    private Tilemap zombieMap;
-    [SerializeField]
-    private Tilemap codeMap;
-    [SerializeField]
-    private TileBase zombieTile;
-     [SerializeField]
-    private TileBase mineTile;
-    [SerializeField]
-    private PlayerMovement controls;
-    [SerializeField]
-    private Rigidbody2D player;
-    [SerializeField]
-    private Vector2Int position;
-    
-    // private bool canMove;
-    // private int presses = 0;
-    private String  currKey;
-    private void Awake(){
-        controls = new PlayerMovement();
-    }
+  [SerializeField]
+  private Tilemap grassMap;
+  [SerializeField]
+  private Tilemap zombieMap;
+  [SerializeField]
+  private Tilemap codeMap;
+  [SerializeField]
+  private TileBase zombieTile;
+  [SerializeField]
+  private TileBase mineTile;
+  [SerializeField]
+  private PlayerMovement controls;
+  [SerializeField]
+  private Rigidbody2D player;
+  [SerializeField]
+  private Tilemap flagMap;
+  [SerializeField]
+  private TileBase flagTile;
+  [SerializeField]
+  private Vector2Int position;
 
-    private void OnDisable(){
-        controls.Disable();
-    }
-    private void OnEnable(){
-        controls.Enable();
-    }
-    // Start is called before the first frame update
-    void Start()
+  private Vector2 currDirection;
+  private int presses = 0;
+
+  private bool lose = false;
+  private bool activeFlag = false;
+
+  private void Awake()
+  {
+    // controls = new PlayerMovement();
+  }
+
+  private void OnDisable()
+  {
+    // controls.Disable();
+  }
+  private void OnEnable()
+  {
+    // controls.Enable();
+  }
+  // Start is called before the first frame update
+  void Start()
+  {
+    // controls.main.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+
+
+    // this is whats next thursday
+
+    // controls.main.Actions.performed += ctx => ;
+    // player.MovePosition(position);
+    // compress bounds for num rows and num cols, save value
+    // adjustable amount of zombies for difficulty
+    // create vector3 for cell location of zombie spawn, then set tile to zombie
+    // be mindful of offset, fix in unity to center first (unless i can fix that problem here)
+    // check if zombie is too close, while loop until fixed
+    grassMap.CompressBounds();
+    var size = grassMap.size;
+    var numZombies = 5;
+    for (var i = 0; i < numZombies; i++)
     {
-        controls.main.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
 
-
-// this is whats next thursday
-
-        // controls.main.Actions.performed += ctx => ;
-        player.MovePosition(position);
-         // compress bounds for num rows and num cols, save value
-        // adjustable amount of zombies for difficulty
-        // create vector3 for cell location of zombie spawn, then set tile to zombie
-        // be mindful of offset, fix in unity to center first (unless i can fix that problem here)
-        // check if zombie is too close, while loop until fixed
-        grassMap.CompressBounds();
-        var size = grassMap.size;
-        var numZombies = 5;
-        for(var i = 0; i < numZombies; i ++){
-           
-            var random = new Vector3Int(Random.Range(1,size.x)-7, Random.Range(1,size.y)-6, 0 );
-            while (Math.Abs(position.x - random.x) < 4 && Math.Abs(position.y-random.y) < 4) {
-                random = new Vector3Int(Random.Range(1,size.x)-7, Random.Range(1,size.y)-6, 0 );
-            }
-          zombieMap.SetTile(random,zombieTile);
-        }
+      var random = new Vector3Int(Random.Range(1, size.x) - 7, Random.Range(1, size.y) - 6, 0);
+      while (Math.Abs(position.x - random.x) < 4 && Math.Abs(position.y - random.y) < 4)
+      {
+        random = new Vector3Int(Random.Range(1, size.x) - 7, Random.Range(1, size.y) - 6, 0);
+      }
+      zombieMap.SetTile(random, zombieTile);
     }
+  }
 
-    private void Move(Vector2 direction){
-        if (CanMove(direction)){
-            transform.position += (Vector3)direction;
-        }
-    }
-
-    private bool CanMove(Vector2 direction){
-        var position =  grassMap.WorldToCell(transform.position + (Vector3)direction);
-        if (!codeMap.HasTile(position)||grassMap.HasTile(position)) {
-            Debug.Log("can't go that way.");
-           return false;
-        }
-        
-       return true;
-    }   
-    // Update is called once per frame
-    void Update()
+  private void Move(Vector2 direction)
+  {
+    if (CanMove(direction))
     {
-      if( Input.GetKeyUp("f")){
-        currKey = "f";
-        // if( grassMap.HasTile()){}
-      }
-      else if( Input.GetKeyUp(KeyCode.UpArrow)){
-        currKey = "u";
-      }
-      else if( Input.GetKeyUp(KeyCode.LeftArrow)){
-        currKey = "l";
-      }
-      else if( Input.GetKeyUp(KeyCode.DownArrow)){
-        currKey = "d";
-      }
-      else if( Input.GetKeyUp(KeyCode.RightArrow)){
-        currKey = "r";
-      }
-      Debug.Log(currKey);
-       if (zombieMap.HasTile(codeMap.WorldToCell(transform.position))){
-            Debug.Log("WAAAAGHH");
-        }
-        else if(codeMap.GetTile(codeMap.WorldToCell(transform.position)).Equals(mineTile)){
-            Debug.Log("BOOM");
-        }
-    
+      transform.position += (Vector3)direction;
     }
+  }
+
+  private bool CanMove(Vector2 direction)
+  {
+    var position = grassMap.WorldToCell(transform.position + (Vector3)direction);
+    if (!codeMap.HasTile(position) || grassMap.HasTile(position) || flagMap.HasTile(position))
+    {
+      return false;
+    }
+
+    return true;
+  }
+  private void tryMove(Vector2Int direction)
+  {
+    presses += 1;
+    if (presses == 1)
+    {
+      currDirection = direction;
+      activeFlag = true;
+    }
+    if (presses == 2)
+    {
+      if (direction == currDirection)
+      {
+        Move(direction);
+        presses = 0;
+        activeFlag = false;
+      }
+      else
+      {
+        currDirection = direction;
+        presses = 1;
+        activeFlag = true;
+      }
+    }
+    Debug.Log(presses);
+  }
+
+  private void tryDig()
+  {
+    if (activeFlag)
+    {
+      // var newPlace = new Vector3Int((int)transform.position.x/2 + currDirection.x, (int)transform.position.y + currDirection.y , 0);
+      if (codeMap.GetTile(codeMap.WorldToCell(transform.position + (Vector3)currDirection)).Equals(mineTile))
+      {
+        grassMap.SetTile(codeMap.WorldToCell(transform.position + (Vector3)currDirection), null);
+      }
+      else
+      {
+        grassMap.SetTile(codeMap.WorldToCell(transform.position + (Vector3)currDirection), null);
+      }
+    }
+  }
+  private void flag()
+  {
+    if (activeFlag)
+    {
+      if (!flagMap.HasTile(codeMap.WorldToCell(transform.position)))
+      {
+        flagMap.SetTile(flagMap.WorldToCell(transform.position + (Vector3)currDirection), flagTile);
+      }
+      else
+      {
+        flagMap.SetTile(flagMap.WorldToCell(transform.position + (Vector3)currDirection), null);
+      }
+    }
+
+  }
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetButtonUp("Horizontal"))
+    {
+      if (Input.GetAxis("Horizontal") > 0)
+      {
+        tryMove(new Vector2Int(1, 0));
+      }
+      else
+      {
+        tryMove(new Vector2Int(-1, 0));
+      }
+    }
+    else if (Input.GetButtonUp("Vertical"))
+    {
+      if (Input.GetAxis("Vertical") < 0)
+      {
+        tryMove(new Vector2Int(0, -1));
+      }
+      else
+      {
+        tryMove(new Vector2Int(0, 1));
+      }
+    }
+    else if (Input.GetButtonUp("V"))
+    {
+      tryDig();
+    }
+    else if (Input.GetButtonUp("F"))
+    {
+      flag();
+    }
+
+
+
+    // if( Input.GetKeyUp("f")){
+    //   currKey = "f";
+    //   // if( grassMap.HasTile()){}
+    // }
+    // else if( Input.GetKeyUp(KeyCode.UpArrow)){
+    //   currKey = "u";
+    // }
+    // else if( Input.GetKeyUp(KeyCode.LeftArrow)){
+    //   currKey = "l";
+    // }
+    // else if( Input.GetKeyUp(KeyCode.DownArrow)){
+    //   currKey = "d";
+    // }
+    // else if( Input.GetKeyUp(KeyCode.RightArrow)){
+    //   currKey = "r";
+    // }
+    // Debug.Log(currKey);
+    if (zombieMap.HasTile(codeMap.WorldToCell(transform.position)))
+    {
+      lose = true;
+    }
+
+
+  }
 }
